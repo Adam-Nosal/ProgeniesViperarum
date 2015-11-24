c8ac00b946162507b9d89c736f199aa22d4fb547
tiscali <tiscali11@o2.pl>
2015-11-25 00:21:28 +0100

    adding movement | walk,shoot,reload, idle animations | need to work on right joystick, setting lookrotationof player

---
+++ UnityProject/Assets/Scripts/Bahaviour/PlayerAnimationController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using Helpers;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private Animator mAnimator;
+
+    #endregion
+
+    #region Variables
+    #endregion
+
+    #region Public Geters
+    #endregion
+
+
+
+    void Awake()
+    {
+        InitVariables();
+        PlayIdleAnimation();
+    }
+
+
+    #region Private Methods
+
+    private void InitVariables()
+    {
+        if (mAnimator == null)
+            mAnimator = this.GetComponent<Animator>();
+
+
+    }
+
+    #endregion
+
+    #region Public Methods
+
+
+    public void PlayReloadAnimation(bool origin)
+    {
+         mAnimator.SetBool(PlayerAnimatorHelper.PLAYER_IS_RELOADING_PARAM, true);
+    }
+
+    public void PlayShootAnimation(bool origin)
+    {
+         mAnimator.SetBool(PlayerAnimatorHelper.PLAYER_IS_SHOOTING_PARAM, true);
+    }
+
+    public void PlayIdleAnimation(){
+
+              mAnimator.SetBool(PlayerAnimatorHelper.PLAYER_IS_SHOOTING_PARAM, false);
+             mAnimator.SetBool(PlayerAnimatorHelper.PLAYER_IS_RELOADING_PARAM, false);
+    }
+
+    public void PlayWalkAnimation(float velocity)
+    {
+        mAnimator.SetFloat(PlayerAnimatorHelper.PLAYER_VELOCITY_PARAM, velocity);
+    }
+
+    #endregion
+
+}
---
+++ UnityProject/Assets/Scripts/Bahaviour/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using CnControls;
+using System.Collections;
+using Helpers;
+
+public class PlayerController : MonoBehaviour
+{
+
+    #region Editor Variables
+    [Header("Controllers")]
+    [SerializeField]
+    private PlayerAnimationController mAnimationController;
+    [SerializeField]
+    private Rigidbody2D mRigidbody2d;
+    [SerializeField]
+    private Transform mTransform;
+
+    [Header("Movement")]
+    [SerializeField]
+    [Range(5.0f,15.0f)]
+    private float Speed = 7.5f;
+
+    #endregion
+
+
+    #region Variables
+    public Vector3 lookVector;
+    public Vector3 destinationRotation;
+    #endregion
+
+    #region Public Geters
+    #endregion
+
+
+    void Awake()
+    {
+        InitVariables();
+    }
+
+
+
+    void Update()
+    {
+        mRigidbody2d.velocity = new Vector2(CnInputManager.GetAxis(InputHelper.MOVE_JOYSTICK_HORIZONTAL)*Speed,
+                                            CnInputManager.GetAxis(InputHelper.MOVE_JOYSTICK_VERTICAL) * Speed);
+
+        lookVector = new Vector3(CnInputManager.GetAxis(InputHelper.LOOK_JOYSTICK_HORIZONTAL) ,
+                                            CnInputManager.GetAxis(InputHelper.LOOK_JOYSTICK_VERTICAL) ,0.0f);
+
+        destinationRotation = Quaternion.FromToRotation(transform.position, lookVector).eulerAngles;
+        destinationRotation.x = 0.0f;
+        destinationRotation.y = 0.0f;
+        mTransform.rotation = Quaternion.Euler(destinationRotation);
+
+
+
+        //animation handling
+        if (mRigidbody2d.velocity.x > 0 )
+            mAnimationController.PlayWalkAnimation(mRigidbody2d.velocity.x);
+        else
+        if ( mRigidbody2d.velocity.y > 0)
+            mAnimationController.PlayWalkAnimation(mRigidbody2d.velocity.y);
+        else
+            mAnimationController.PlayWalkAnimation(0.0f);
+        //TODO: implement different walk animation speed if player speed is high 
+    
+    }
+
+
+    #region Private Methods
+
+    private void InitVariables()
+    {
+
+        if (mAnimationController == null)
+            mAnimationController = this.GetComponent<PlayerAnimationController>();
+        if (mRigidbody2d == null)
+            mRigidbody2d = this.GetComponent<Rigidbody2D>();
+        if (mTransform == null)
+            mTransform = this.GetComponent<Transform>();
+    }
+
+    #endregion
+
+
+
+    #region Public Methods
+
+
+    #endregion
+
+}
